0072f88ee0599fe45c31a311a3240ed7c7585a3f
Liam-Walton19074402 <19074402@stu.mmu.ac.uk>
2021-03-29 12:52:32 +0100

    Add Workspace Directories and GenKeys.java

--- Workspace/src/GenKeys.java
+++ Workspace/src/GenKeys.java
@@ -23,14 +23,31 @@ public class GenKeys {
 	}//End of generateSecureRandom
 	
 	/**
-	 * 
+	 * This function is responsible for generating the key pair for crypto 
 	 */
 	public void generateKeys() {
 		try {
 			keyGen = KeyPairGenerator.getInstance("AES");
+			keyGen.initialize(256, sr);;
+			kp = keyGen.generateKeyPair();
 		} catch (NoSuchAlgorithmException e) {
-			
 			e.printStackTrace();
 		}//End of try/catch
+	}//End of generateKeys
+	
+	/**
+	 *  This function returns the private key from the key pair
+	 * @return PrivateKey: The private key of the key pair
+	 */
+	public PrivateKey getPrivKey() {
+		return kp.getPrivate();
+	}//End of getPrivKey
+	
+	/**
+	 *  This function returns the public key from the key pair
+	 * @return PublicKey: The public key of the key pair
+	 */
+	public PublicKey getPubKey() {
+		return kp.getPublic();
 	}
-}
+}//End of GenKeys
