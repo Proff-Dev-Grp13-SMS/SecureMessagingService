4bc5703dd6e1ef5638b7b3ae0fdc53346de5d6f5
bismah20 <80458292+bismah20@users.noreply.github.com>
2021-04-18 14:51:06 +0100

    Update LoginGUI.java

--- Workspace/src/LoginGUI.java
+++ Workspace/src/LoginGUI.java
@@ -30,6 +30,8 @@ public class LoginGUI implements ActionListener {		//public class that implement
 	{
 		JFrame frame = new JFrame();		//new 'window' basically, e.g. the login page
 		JPanel panel = new JPanel();		//panel that you can add stuff into e.g. like buttons and labels
+		JTextField LoginText = new JTextField (20); //adds a text field for login 
+		JTextField SignUpText = new JTextField(20);
 		JButton button;				//one button
 		JButton button2;			//two button
 		button = new JButton("Login");		//New button "sign up", will need position moved
@@ -38,20 +40,23 @@ public class LoginGUI implements ActionListener {		//public class that implement
 		button2.addActionListener(this);	// ^^^^^^^
 		
 		label = new JLabel("Number of clicks: 0");	//label / text that is displayed to users
-		
+		LoginText.setBounds(50,50,710,1270); //sets the position and the layout of the text field 
+                LoginText.setBounds(70,70,760,1290);  
 		
        
 		
 		
 		panel.setBorder(BorderFactory.createEmptyBorder(60, 60, 720, 1280)); //x, y, width, height (border of the objects)
-		panel.setLayout(new GridLayout(0, 1));		//fuck knows what this is tbh but it's important apparently
+		panel.setLayout(new GridLayout(0, 1));		////a grid layout, creating one column 
 		panel.setBackground(Color.gray);		//background colour
 		panel.add(button);				//add button to the panel
+		panel.add(LoginText); //adds the panel 
+		panel.add(SignUpText); //this adds the 2nd text field underneath the login button but we need to change the layout of the buttons 
 		panel.add(button2);				// ^^^^^^^^^^^^^
 		
 		 
 		panel.add(label);				//add our label to the panel
-		label.setBounds(50, 50, 50, 50);		//maybe the boundaries / position of the label ? not sure
+		label.setBounds(50, 50, 50, 50);		//position of the label 
 		
 		frame.add(panel, BorderLayout.CENTER);			//add the above panels to the frame and centre it I think ?
 		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);		//closes the frame when you close the program so it is no longer loaded
