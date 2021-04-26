727a1ae9f0b122750cd77f27ec91892a0fa24c77
Liam Walton <LWalton874@gmail.com>
2021-04-26 17:02:42 +0100

    Add Crypto.java

---
+++ Workspace/src/SMS/Crypto.java
@@ -0,0 +1,12 @@
+package SMS;
+
+import javax.crypto.SecretKey;
+
+/**
+ * Crypto Class
+ * @author Liam Walton
+ * This class is responsible for encrypting data for the application.
+ */
+public class Crypto extends GenKeys {
+	
+}
