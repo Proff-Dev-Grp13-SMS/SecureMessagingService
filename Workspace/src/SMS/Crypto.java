57b805e5492369e2b332dde8c3df18325ab7a815
Liam Walton <LWalton874@gmail.com>
2021-04-27 09:50:39 +0100

    Add getKeySpecPRK function to Crypto.java

--- Workspace/src/SMS/Crypto.java
+++ Workspace/src/SMS/Crypto.java
@@ -3,8 +3,10 @@ package SMS;
 import java.security.InvalidKeyException;
 import java.security.KeyFactory;
 import java.security.NoSuchAlgorithmException;
+import java.security.PrivateKey;
 import java.security.PublicKey;
 import java.security.spec.InvalidKeySpecException;
+import java.security.spec.PKCS8EncodedKeySpec;
 import java.security.spec.X509EncodedKeySpec;
 import java.util.Base64;
 
@@ -22,25 +24,51 @@ import javax.crypto.NoSuchPaddingException;
  */
 public class Crypto extends GenKeys {
 	/**
-	 * Function is responsible for getting the key spec of the Public Key Generated for the user
+	 * Function is responsible for making a usable PublicKey for the user
 	 * @return PublicKey returnMe: A usable public key for encryption
 	 */
 	private static PublicKey getKeySpecPK() {
 		PublicKey myKey = kp.getPublic();//Key to get bytes from
 		PublicKey returnMe = null;//New Key to return
+		
 		try {
 			//Create KeySpec for Cipher
 			X509EncodedKeySpec keySpec = new X509EncodedKeySpec(Base64.getDecoder().decode(myKey.getEncoded()));
 			KeyFactory keyFactory = KeyFactory.getInstance("RSA");
 			returnMe = keyFactory.generatePublic(keySpec);
+			
 			return returnMe;
+			
 		} catch (NoSuchAlgorithmException e) {
             e.printStackTrace();
         } catch (InvalidKeySpecException e) {
             e.printStackTrace();
         }
+		
 		return returnMe;
 	}//End of getKeySpecPK
+	
+	/**
+	 * Function is responsible for making a usable PrivateKey
+	 * @return PrivateKey returnMe: A usable private key for decryption
+	 */
+	private static PrivateKey getKeySpecPRK() {
+		PrivateKey myKey = kp.getPrivate();//Key to convert
+		PrivateKey returnMe = null;//Key to return
+		
+		try {
+			//Create KeySpec object
+			PKCS8EncodedKeySpec keySpec = new PKCS8EncodedKeySpec(Base64.getDecoder().decode(myKey.getEncoded()));
+			KeyFactory keyFactory = KeyFactory.getInstance("RSA");
+			returnMe = keyFactory.generatePrivate(keySpec);
+		} catch (NoSuchAlgorithmException e) {
+            e.printStackTrace();
+        } catch (InvalidKeySpecException e) {
+            e.printStackTrace();
+        }
+		
+		return returnMe;
+	}
 	
 	/**
 	 * This function takes in a string of data and encrypts it
